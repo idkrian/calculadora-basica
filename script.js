b3795c7c116dbbfdd97eaf14ac3fdec0452b8ec7
Rian <riansouza0@gmail.com>
2020-11-13 22:10:37 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function calcular(tipo, valor){
         //limpar o visor
         document.getElementById('resultado').value = ''
       }
+      //Valores de ações 
       if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
         document.getElementById('resultado').value += valor
       }
@@ -11,6 +12,7 @@ function calcular(tipo, valor){
         var valor_campo = eval(document.getElementById('resultado').value)
         document.getElementById('resultado').value = valor_campo
       }
+      //Valores de números
     } else if (tipo === 'valor') {
 
       document.getElementById('resultado').value += valor
